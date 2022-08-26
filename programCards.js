f884c1f239d66d2de427f2a6378a33964b0dff1a
Oualid <oualidchaouati3@gmail.com>
2022-08-26 16:48:33 +0200

    ready.0

---
+++ programCards.js
@@ -0,0 +1,82 @@
+const programCardsArray = [
+  {
+    logo: `<svg width="48" height="47" viewBox="0 0 48 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M24 8.11749L28.845 3.27249L44.1875 18.615L39.3425 23.46L24 8.11749Z" fill="white"/>
+        <path d="M33.69 4.07999L36.92 0.849991L46.61 10.54L43.38 13.77L33.69 4.07999Z" fill="white"/>
+        <path d="M1.39 36.38L4.62 33.15L14.31 42.84L11.08 46.07L1.39 36.38Z" fill="white"/>
+        <path d="M0.582496 43.6475L3.005 41.225L5.83125 44.0512L3.40875 46.4737L0.582496 43.6475Z" fill="white"/>
+        <path d="M41.765 2.46499L44.1875 0.0424914L47.0138 2.86874L44.5913 5.29124L41.765 2.46499Z" fill="white"/>
+        <path d="M3.8125 28.305L8.6575 23.46L24 38.8025L19.155 43.6475L3.8125 28.305Z" fill="white"/>
+        <path d="M14.7137 30.3237L30.8638 14.1737L33.2863 16.5962L17.1362 32.7462L14.7137 30.3237Z" fill="white"/>
+        </svg>
+        `,
+    title: `Flex muscle`,
+    text: `creating tension that’s temporarily making the muscle  fibres smaller or contracted `,
+  },
+  {
+    logo: `<svg width="62" height="54" viewBox="0 0 62 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M42.8863 27.2555H38.1136L34.5341 38.0509L27.375 16.46L23.7954 27.2555H19.0227" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M55.5084 6.88038C54.0923 5.479 52.411 4.36732 50.5604 3.60886C48.7099 2.8504 46.7264 2.46002 44.7233 2.46002C42.7202 2.46002 40.7367 2.8504 38.8861 3.60886C37.0356 4.36732 35.3542 5.479 33.9381 6.88038L30.9992 9.78739L28.0604 6.88038C25.2 4.05101 21.3204 2.46149 17.2752 2.46149C13.23 2.46149 9.35046 4.05101 6.49006 6.88038C3.62966 9.70976 2.02271 13.5472 2.02271 17.5485C2.02271 21.5499 3.62966 25.3873 6.49006 28.2167L9.42894 31.1237L30.9992 52.46L52.5695 31.1237L55.5084 28.2167C56.9252 26.816 58.049 25.1529 58.8158 23.3224C59.5826 21.4919 59.9773 19.5299 59.9773 17.5485C59.9773 15.5672 59.5826 13.6052 58.8158 11.7747C58.049 9.94422 56.9252 8.28111 55.5084 6.88038V6.88038Z" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        
+        `,
+    title: `Cardio exercice`,
+    text: `Excercice your heart rate up and keeps it up for a prolonged period of time`,
+  },
+  {
+    logo: `<svg width="50" height="52" viewBox="0 0 50 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="25" cy="7.16268" r="5.50606" fill="white" stroke="white" stroke-width="2"/>
+        <path d="M13.5623 22.1497C17.2317 14.2123 20.2505 15.8643 24.3623 15.7217C31.8053 15.4635 33.3927 17.179 36.0472 22.1497L40.1851 29.4104C40.9485 30.1738 43.5422 32.5333 47.3157 36.1767C49.907 38.6786 45.468 42.1883 43.5422 40.2105L36.0472 33.3921L32.0915 26.782L31.6752 39.716C32.612 40.1151 35.7714 41.3139 40.9138 42.917C45.442 44.3286 42.3451 50.9585 39.0921 50.2038L24.4924 46.9508C22.7315 47.3585 17.8042 48.6423 10.4654 49.7354C5.53928 50.469 5.49475 43.9059 8.69574 42.5006L17.8042 39.716L17.2317 26.782L13.5623 33.0798C12.2958 34.4504 9.17979 37.6965 6.84802 39.716C3.63281 42.5006 0.0556824 39.716 2.9704 35.8384L9.76273 29.0461C10.2659 28.1092 11.7302 25.4183 13.5623 22.1497Z" fill="white" stroke="white" stroke-width="2"/>
+        </svg>
+        
+        
+        `,
+    title: `Basic yoga`,
+    text: `Diaphragmatic this is the most common breathing technique you’ll find in yoga `,
+  },
+  {
+    logo: `<svg width="58" height="53" viewBox="0 0 58 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M39.3489 11.2803C31.2989 9.67224 26.9854 9.60493 19.7153 11.2803C18.4834 20.3631 24.9766 25.8256 26.6448 26.0866H32.8044C35.2682 25.147 39.7339 14.9343 39.3489 11.2803Z" stroke="white" stroke-width="2"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M9.92951 7.45452L8.74478 1.5583L1.6364 3.13062L4.40077 18.8539L11.5092 17.6746L10.3244 11.3853L13.8786 10.9923C13.2204 14.399 15.5285 25.3004 22.4789 30.3319L24.0585 35.4419L19.3196 44.4828L7.8672 51.5583H16.5552L22.4789 47.6275L27.6127 41.7313C31.0353 40.29 37.9593 38.8224 38.2753 44.4828V51.1652H43.0142C43.885 46.4482 43.4456 36.7784 34.7211 35.835L35.9058 29.5457C38.4069 27.1872 44.901 17.9105 43.3214 10.9923L47.6654 11.3853L46.4806 17.6746L53.589 18.8539L56.3534 2.34446L49.245 1.5583L48.4552 7.84761C41.7417 6.27528 17.8277 4.9388 9.92951 7.45452ZM19.7153 11.2803C26.9854 9.60487 31.2989 9.67219 39.3489 11.2803C39.7339 14.9343 35.2682 25.147 32.8044 26.0866H26.6448C24.9766 25.8256 18.4834 20.3631 19.7153 11.2803Z" fill="white"/>
+        <circle cx="28.9949" cy="18.5394" r="3.71698" fill="white" stroke="white" stroke-width="2"/>
+        </svg>
+        
+        
+        
+        `,
+    title: `Weight lifting`,
+    text: `Attempt a maximum weight sinlge lift  of a barbel loaded with weight platos `,
+  },
+];
+
+const cardsWrapper = document.querySelector(".program-section .cards-wrapper");
+
+programCardsArray.forEach((cardDic) => {
+  const div = document.createElement("div");
+  div.className = "card";
+
+  const topIconWrapper = document.createElement("div");
+  topIconWrapper.className = "icon-wrapper flex f-center";
+  topIconWrapper.innerHTML = cardDic.logo;
+
+  const title = document.createElement("h3");
+  title.className = "title ";
+  title.innerHTML = cardDic.title;
+
+  const text = document.createElement("p");
+  text.className="txt-lg"
+  text.innerHTML = cardDic.text;
+
+  const btn = document.createElement("button");
+  btn.className = "btn flex f-center";
+  const iconWrapper = document.createElement("div");
+  iconWrapper.className = "icon-arrow";
+  btn.appendChild(iconWrapper);
+
+  div.appendChild(topIconWrapper);
+  div.appendChild(title);
+  div.appendChild(text);
+  div.appendChild(btn);
+
+  cardsWrapper.appendChild(div);
+});
